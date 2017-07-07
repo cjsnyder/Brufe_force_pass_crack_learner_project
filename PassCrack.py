b5ccded52076a2193b27810a07482c739a6d3181
cjsnyder <cjsnyder1018@gmail.com>
2017-07-07 16:13:13 -0500

    Made a basic function, works well.

--- PassCrack.py
+++ PassCrack.py
@@ -3,12 +3,16 @@ Purpose - Demonstrate basic concepts and show progress towards larger projects
 Suggested by: Sydni from FBLA """
 import string
 
-finalPass = ""
 def checkPass(password):
-    for letter in password:
-        if letter in string.printable:
-            finalPass += letter
-    return finalPass
+    finalPass = ""
+    while len(finalPass) < len(password):
+        for letter in password:
+            if letter in string.printable:
+                finalPass += letter
+            print("Letter found: " + letter)
+            print("Password currenly is: " + finalPass)
+    print("\nCracked password is: " + finalPass)
+
 
 userPassword = input("Enter your password: ")
-print(checkPass(userPassword))
+checkPass(userPassword)
